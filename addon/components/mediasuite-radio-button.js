15fab6c0c9fb13c2a504f6f1ee2ccbadf251bf85
jonathanh <jonforest@gmail.com>
2016-09-10 15:28:02 +1200

    Radio buttons moved over to ember-one-way-controls

--- addon/components/mediasuite-radio-button.js
+++ addon/components/mediasuite-radio-button.js
@@ -1,5 +1,5 @@
 import Ember from 'ember'
-const { computed, set } = Ember
+const { computed, set, get } = Ember
 
 export default Ember.Component.extend({
   classNames: ['element-wrap', 'radio-button'],
@@ -7,20 +7,12 @@ export default Ember.Component.extend({
   inputID: computed('elementId', function () {
     return `input-${this.get('elementId')}`
   }),
-  checked: false,
   disabled: false,
-  isChecked: computed('checked', function () {
-    return this.get('checked') ? 'checked' : ''
-  }),
   isDisabled: computed('disabled', function () {
     return this.get('disabled') ? 'disabled' : ''
   }),
-  value: '',
-  click (e) {
-    if (this.$(e.target).is('div')) return false
-    if (!this.get('disabled')) {
-      set(this, 'checked', true)
-      this.get('onChange')(this.get('value'))
-    }
-  }
+  isChecked: computed('value', 'option', function () {
+    return get(this, 'value') === get(this, 'option')
+  }),
+  value: ''
 })
