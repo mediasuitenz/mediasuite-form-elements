5e59a20b0e4773f49010652bf382af72ccbe9e0b
jonathanh <jonforest@gmail.com>
2016-09-14 20:07:57 +1200

    Single checkbox implemented

--- addon/components/mediasuite-check-box.js
+++ addon/components/mediasuite-check-box.js
@@ -1,16 +1,10 @@
 import Ember from 'ember'
-const {computed, observer} = Ember
+const {computed} = Ember
 
 export default Ember.Component.extend({
   classNames: ['element-wrap', 'checkbox'],
   classNameBindings: ['useButtonStyle', 'checked'],
-  inputID: computed('elementId', function () {
+  inputId: computed('elementId', function () {
     return `input-${this.get('elementId')}`
-  }),
-  observeCheckbox: observer('checked', function () {
-    const update = this.get('update')
-    if (update) {
-      update(this.get('checked'))
-    }
   })
 })
