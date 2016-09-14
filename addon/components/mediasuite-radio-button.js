25e769c9d69f182397b599ca5b4bea4174593e42
jonathanh <jonforest@gmail.com>
2016-09-14 20:04:11 +1200

    Fix for is-checked class on radio buttons

--- addon/components/mediasuite-radio-button.js
+++ addon/components/mediasuite-radio-button.js
@@ -3,13 +3,16 @@ const { computed, get } = Ember
 
 export default Ember.Component.extend({
   classNames: ['element-wrap', 'radio-button'],
-  classNameBindings: ['isChecked:is-checked'],
-  inputID: computed('elementId', function () {
+  classNameBindings: ['isChecked'],
+  inputId: computed('elementId', function () {
     return `input-${get(this, 'elementId')}`
   }),
   disabled: false,
   isDisabled: computed('disabled', function () {
     return get(this, 'disabled') ? 'disabled' : ''
+  }),
+  isChecked: computed('value', 'option', function () {
+    return get(this, 'value') === get(this, 'option')
   }),
   value: ''
 })
